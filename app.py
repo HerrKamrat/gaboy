50f4c021d0e895dec7c03b5dfc04a25ac33d658a
Gabriel Sulka <gabsul@bredband2.se>
2024-10-24 21:19:51 +0200

    Init

--- app.py
+++ app.py
@@ -2,34 +2,20 @@
 
 import pygame, time, select, math
 
-# Very important: the exact pixel size of the TFT screen must be known so we can build graphics at this exact format
 surfaceSize = (240, 240)
+fontFile = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
 
-# Note that we don't instantiate any display!
 pygame.init()
+pygame.font.init()
 
-# The pygame surface we are going to draw onto.
-# /!\ It must be the exact same size of the target display /!\
+defaultFont = pygame.font.Font(fontFile, 30)
 lcd = pygame.Surface(surfaceSize)
 
-# This is the important bit
 def refresh():
-    # We open the TFT screen's framebuffer as a binary file. Note that we will write bytes into it, hence the "wb" operator
     f = open("/dev/fb1","wb")
-    # According to the TFT screen specs, it supports only 16bits pixels depth
-    # Pygame surfaces use 24bits pixels depth by default, but the surface itself provides a very handy method to convert it.
-    # once converted, we write the full byte buffer of the pygame surface into the TFT screen framebuffer like we would in >
     f.write(lcd.get_buffer())
-    # We can then close our access to the framebuffer
     f.close()
     time.sleep(0.016)
-
-# Now we've got a function that can get the bytes from a pygame surface to the TFT framebuffer,
-# we can use the usual pygame primitives to draw on our surface before calling the refresh function.
-
-# Here we just blink the screen background in a few colors with the "Hello World!" text
-pygame.font.init()
-defaultFont = pygame.font.SysFont(None,30)
 
 lcd.fill((255,0,0))
 lcd.blit(defaultFont.render("Hello World!", False, (0, 0, 0)),(0, 0))
